62d40ec13811fe33db3c26fce946d732c751ba85
Oleksii Tyshchenko <alexcare1337@gmail.com>
2024-07-11 23:29:14 +0300

    🔧 Set up webhook

--- index.js
+++ index.js
@@ -1,86 +1,89 @@
-import 'dotenv/config'
-import TelegramBot from "node-telegram-bot-api";
-import { onStart } from "./events/start.js";
-import { onCreatePoll } from "./events/createPoll.js";
-import { onPollAnswer } from "./events/pollAnswer.js";
-import { onCallbackQuery, onSkill } from "./events/skill.js";
-import { onCustomPlayer } from "./events/customPlayer.js";
-import { onGenerateTeams } from "./events/generateTeams.js";
-import { onPlayers } from "./events/players.js";
-import { onAll } from "./events/all.js";
-import express from "express";
-import bodyParser from "body-parser";
-import { performBotAction } from "./utils.js";
+import 'dotenv/config';
+import TelegramBot from 'node-telegram-bot-api';
+import { onStart } from './events/start.js';
+import { onCreatePoll } from './events/createPoll.js';
+import { onPollAnswer } from './events/pollAnswer.js';
+import { onCallbackQuery, onSkill } from './events/skill.js';
+import { onCustomPlayer } from './events/customPlayer.js';
+import { onGenerateTeams } from './events/generateTeams.js';
+import { onPlayers } from './events/players.js';
+import { onAll } from './events/all.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { performBotAction } from './utils.js';
+//import { PrismaClient } from '@prisma/client';
+
+//export const prisma = new PrismaClient();
 
 const token = process.env.TELEGRAM_TOKEN;
 
 // dev
-const bot = new TelegramBot(token, { polling: true });
+//const bot = new TelegramBot(token, { polling: true });
 
 // prod
-//const url = 'https://football-bot-psi.vercel.app'; // Add this environment variable in Vercel project settings
+const url = 'https://split-teams-telegram-bot.onrender.com'; // Add this environment variable
 
-//const bot = new TelegramBot(token, { webHook: true });
+const bot = new TelegramBot(token, { webHook: true });
 
-//(async () => {
-//  try {
-//    await bot.setWebHook(`${url}/api/bot`);
-//    console.log(`Webhook set to: ${url}/api/bot`);
-//  } catch (error) {
-//    console.error('Error setting webhook:', error);
-//  }
-//})();
+(async () => {
+  try {
+    await bot.setWebHook(`${url}/api/bot`);
+    console.log(`Webhook set to: ${url}/api/bot`);
+  } catch (error) {
+    console.error('Error setting webhook:', error);
+  }
+})();
 
 export const allPlayers = {
-  "aleksey_10 (Oleksii)": {
+  'aleksey_10 (Oleksii)': {
     level: 2,
   },
-  "whoistwixy (bodya)": {
+  'whoistwixy (bodya)': {
     level: 1,
   },
-  "nik01314 (Никита)": {
+  'nik01314 (Никита)': {
     level: 2,
   },
-  "voxat1488 (Вохa☠️)": {
+  'voxat1488 (Вохa☠️)': {
     level: 2,
   },
-  "Prontik (Artem Prontenko)": {
+  'Prontik (Artem Prontenko)': {
     level: 1,
   },
-  "djsvej (DJ SVEJ)": {
+  'djsvej (DJ SVEJ)': {
     level: 1,
   },
-  "traxxse (krisong)": {
+  'traxxse (krisong)': {
     level: 0,
   },
-  "maksvatsiuk (Makc Vatsiuk)": {
+  'maksvatsiuk (Makc Vatsiuk)': {
     level: 0,
   },
-  "omelchenkodmitriy (Дмитрій Омельченко)": {
+  'omelchenkodmitriy (Дмитрій Омельченко)': {
     level: 1,
   },
-  "ice_vb (Valeriy Bozhenko)": {
+  'ice_vb (Valeriy Bozhenko)': {
     level: 2,
   },
-  "vesely4ak (whatislove)": {
+  'vesely4ak (whatislove)': {
     level: 0,
   },
-  "mk_nolimits (Макар)": {
+  'mk_nolimits (Макар)': {
     level: 0,
   },
-  "Apso110 (А)": {
+  'Apso110 (А)': {
     level: 2,
   },
-  "Danyakrya (Даня Левченко)": {
+  'Danyakrya (Даня Левченко)': {
     level: 1,
   },
-  "sanyaluchdance58 (Сашка)": {
+  'sanyaluchdance58 (Сашка)': {
     level: 1,
   },
   Влад: {
     level: 0,
   },
-  "envoyeed (Андрей)": {
+  'envoyeed (Андрей)': {
     level: 0,
   },
   Олег: {
@@ -92,7 +95,7 @@ export const allPlayers = {
   Ярик: {
     level: 1,
   },
-  "bomjkolydun (Степаша 🧐)": {
+  'bomjkolydun (Степаша 🧐)': {
     level: 2,
   },
   Artem: {
@@ -101,25 +104,25 @@ export const allPlayers = {
   Олександр: {
     level: 2,
   },
-  "Tretyak27 (Влад)": {
+  'Tretyak27 (Влад)': {
     level: 1,
   },
-  "SnimaemSanechku (Саня)": {
+  'SnimaemSanechku (Саня)': {
     level: 1,
   },
-  "brs117 (Андрей)": {
+  'brs117 (Андрей)': {
     level: 0,
   },
-  "bytexs (🥷🏻)": {
+  'bytexs (🥷🏻)': {
     level: 1,
   },
-  "ospayne (Макс)": {
+  'ospayne (Макс)': {
     level: 0,
   },
-  "vitaliy9386 (Виталий)": {
+  'vitaliy9386 (Виталий)': {
     level: 2,
   },
-  "Dimtet (Дім)": {
+  'Dimtet (Дім)': {
     level: 2,
   },
   Сёма: {
@@ -128,44 +131,44 @@ export const allPlayers = {
 };
 
 export const pollResults = {
-  "-1001930766161": {
-    "djsvej (DJ SVEJ)": 0,
-    "Prontik (Artem Prontenko)": 1,
-    "aleksey_10 (Oleksii)": 1,
-    "vesely4ak (whatislove)": 0,
-    "maksvatsiuk (Makc Vatsiuk)": 0,
+  '-1001930766161': {
+    'djsvej (DJ SVEJ)': 0,
+    'Prontik (Artem Prontenko)': 1,
+    'aleksey_10 (Oleksii)': 1,
+    'vesely4ak (whatislove)': 0,
+    'maksvatsiuk (Makc Vatsiuk)': 0,
     Ярик: 0,
-    "Zxcsobachka (Zxc_sobachka🖤⌯)": 1,
-    "whoistwixy (bodya)": 1,
-    "sanyaluchdance58 (Сашка)": 1,
-    "DenysKulikov (Денис)": 0,
-    "ice_vb (Valeriy Bozhenko)": 1,
-    "nik01314 (Никита)": 0,
-    "voxat1488 (Вохa☠️)": 2,
+    'Zxcsobachka (Zxc_sobachka🖤⌯)': 1,
+    'whoistwixy (bodya)': 1,
+    'sanyaluchdance58 (Сашка)': 1,
+    'DenysKulikov (Денис)': 0,
+    'ice_vb (Valeriy Bozhenko)': 1,
+    'nik01314 (Никита)': 0,
+    'voxat1488 (Вохa☠️)': 2,
     Олександр: 1,
-    "omelchenkodmytrii (Дмитрій Омельченко)": 1,
-    "SnimaemSanechku (Саня)": 0,
-    "ellxc1wn (ㅤ)": 0,
-    "ShevchukArtur (Артур Шевчук)": 1,
-    "Bogdan Tsapenko": 0,
+    'omelchenkodmytrii (Дмитрій Омельченко)': 1,
+    'SnimaemSanechku (Саня)': 0,
+    'ellxc1wn (ㅤ)': 0,
+    'ShevchukArtur (Артур Шевчук)': 1,
+    'Bogdan Tsapenko': 0,
     Олег: 0,
-    "traxxse (krisong)": 0,
+    'traxxse (krisong)': 0,
     Artem: 0,
-    "Danyakrya (Даня Левченко)": 1,
-    "bomjkolydun (Степаша 🧐)": 0,
-    "mk_nolimits (Макар)": 0,
-    "brs117 (Андрей)": 1,
+    'Danyakrya (Даня Левченко)': 1,
+    'bomjkolydun (Степаша 🧐)': 0,
+    'mk_nolimits (Макар)': 0,
+    'brs117 (Андрей)': 1,
     Влад: 1,
-    "bibabobib (Timur)": 1,
+    'bibabobib (Timur)': 1,
     Саенко: 0,
-    "ospayne (Макс)": 0,
-    "bytexs (🥷🏻)": 1,
-    "+1 ще хтось": 0,
-    "Дима Горб": 1,
-    "Apso110 (А)": 0,
+    'ospayne (Макс)': 0,
+    'bytexs (🥷🏻)': 1,
+    '+1 ще хтось': 0,
+    'Дима Горб': 1,
+    'Apso110 (А)': 0,
   },
-  "-4258926914": {
-    "aleksey_10 (Oleksii)": 0,
+  '-4258926914': {
+    'aleksey_10 (Oleksii)': 0,
     вася: 0,
     Олег: 0,
     Сашко: 0,
@@ -175,8 +178,8 @@ export const pollResults = {
 };
 
 export const pollChatMap = new Map().set(
-  "5219974582928149086",
-  "-1001930766161",
+  '5219974582928149086',
+  '-1001930766161'
 );
 
 //fs.readFile('5219974582928149086.txt', 'utf8', (err, data) => {
@@ -207,15 +210,15 @@ export const pollChatMap = new Map().set(
   onPlayers,
   onCallbackQuery,
   onAll,
-].forEach((cb) => cb(bot));
+].forEach(cb => cb(bot));
 
-bot.onText(/\/stop/, (msg) => {
+bot.onText(/\/stop/, msg => {
   const chatId = msg.chat.id;
 
-  performBotAction(() => bot.sendMessage(chatId, "See you later 🫡"));
+  performBotAction(() => bot.sendMessage(chatId, 'See you later 🫡'));
 });
 
-bot.onText(/\/chatid/, (msg) => {
+bot.onText(/\/chatid/, msg => {
   const chatId = msg.chat.id;
 
   performBotAction(() => bot.sendMessage(chatId, chatId));
@@ -238,36 +241,36 @@ Apso110 (А) -- option_low
 Danyakrya (Даня Левченко) -- option_middle`
   .split(/\n/)
   .reduce((acc, current) => {
-    const [username, choise] = current.split(" -- ");
+    const [username, choise] = current.split(' -- ');
 
     const levels = { option_low: 0, option_middle: 1, option_strong: 2 };
 
     return { ...acc, [username]: { level: levels[choise] } };
   }, {});
 
-//const app = express();
-//app.use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json());
 
-//app.post('/api/bot', (req, res) => {
-//  try {
-//    console.log('Received request headers:', req.headers);
-//    console.log('Received request body:', JSON.stringify(req.body, null, 2));
-//    bot.processUpdate(req.body);
-//    res.sendStatus(200);
-//  } catch (error) {
-//    console.error('Error processing update:', error);
-//    res.sendStatus(500);
-//  }
-//});
+app.post('/api/bot', (req, res) => {
+  try {
+    console.log('Received request headers:', req.headers);
+    console.log('Received request body:', JSON.stringify(req.body, null, 2));
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error processing update:', error);
+    res.sendStatus(500);
+  }
+});
 
-//app.get('/', (req, res) => {
-//  res.send('Telegram bot is running');
-//});
+app.get('/', (req, res) => {
+  res.send('Telegram bot is running');
+});
 
-//const port = 5001;
+const port = process.env.PORT || 4000;
 
-//app.listen(port, function () {
-//  console.log('listening on port ' + port);
-//});
+app.listen(port, function () {
+  console.log('listening on port ' + port);
+});
 
-//export default app;
+export default app;
